3a26eca9e932f1be6cfad09534f171f258345095
Nasri Samy <sana62205@eleve.isep.fr>
2022-03-14 16:11:04 +0100

    Added Basic css Added Basic HTML structure

--- index.php
+++ index.php
@@ -1,15 +1,25 @@
-
-
 <!DOCTYPE html>
-<html lang="en">
 <head>
-	<meta charset="utf-8" />
-	<title>Page d'accueil</title>
+    <title>Bienvenue</title>
+    <link rel="stylesheet" href="Accueil.css">
 </head>
 <body>
-	<?php
-
-    echo "hello world"
-    ?>
+    
+    <h1> <span>Bienvenue sur l'espace Care-Workplace </span> </h1>
+    
+    <div id="zone">
+    <div> <img src="logomeasure.png" alt="logomeasure" width="200" height="180" id="logo">  </div>
+    <ul>
+        <li><a href="index.php" class="lienmenu">Accueil</a></li>
+        <li><a href="LeProduit.php" class="lienmenu">Le produit</a></li>
+        <li>Espace Personnel</li>
+        <li>Ludique</li>
+        <li>FAQ</li>
+        <li>Nous contactez</li>     
+    </ul>
+    <button type="button">
+        Connexion
+    </button>
+    </div>
+   
 </body>
-</html>
