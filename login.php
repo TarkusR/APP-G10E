6d44ccdcb5f8e46a72a70aee020ca3ff88bd2581
ALegend4us <alexgendrot14@gmail.com>
2022-03-25 11:06:54 +0100

    updated new style.css

--- login.php
+++ login.php
@@ -115,8 +115,8 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
     ?>
 
     <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
-        <div class="zoneEntree"></div>
-
+        <div class="zoneEntree">
+            <div>
                 <label>Nom de compte</label>
                 <input type="text" name="username" <?php echo (!empty($username_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $username; ?>">
                 <span><?php echo $username_err; ?></span>
