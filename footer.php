802afbd5a35715df92c0c4ccb1e78b3b67513677
ALegend4us <alexgendrot14@gmail.com>
2022-03-25 10:30:11 +0100

    Intégration du fichier style au login

---
+++ footer.php
@@ -0,0 +1,19 @@
+<?php
+?>
+
+<!DOCTYPE html>
+<html>
+<link rel="stylesheet" href="style.css">
+<footer>
+
+    <div class="footer">
+        <h1> Contactez nous </h1>
+        <p> Insta / Facebook / Twitter </p>
+        <h1> Nous trouver </h1>
+        <p1> 28 rue Notre Dame des Champs </p1>
+        <h1> Notre standing de qualité </h1>
+        <img src="WhatsApp Image 2022-03-15 at 19.30.05.jpeg" width="100" height="100">
+    </div>
+
+</footer>
+</html>
