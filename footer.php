295f411402a13ac7fbcdd7f0c5a531fecce3e55d
etiennegrandjean <stepan.grandjean@gmail.com>
2022-03-29 00:22:41 +0200

    Intégration du fichier style au login

--- footer.php
+++ footer.php
@@ -4,11 +4,11 @@
 <footer>
     <div class="footer">
         <h1> Contactez-nous </h1>
-        <a href=""> Instagram</a>
-        <a href=""> Facebook</a>
-        <a href=""> Twitter</a>
+        <p class = "p">
+            <a href=""> Instagram</a> <br> <a href=""> Facebook</a> <br> <a href=""> Twitter</a>
+        </p>
         <h1> Nous trouver </h1>
-        <p1> 28 rue Notre Dame des Champs </p1>
+        <p1 class = "p1"> 28 rue Notre Dame des Champs <br> 75006 <br> PARIS</p1>
     </div>
 </footer>
 
