8a93dfe13700dbf34f994d42948dfd69c7a75754
etiennegrandjean <stepan.grandjean@gmail.com>
2022-03-28 14:58:17 +0200

    Intégration du fichier style au login

--- footer.php
+++ footer.php
@@ -8,17 +8,14 @@
 
     <div class="footer">
         <h1> Contactez-nous </h1>
-        <a href="https://www.instagram.com/alex__gdt/"> Instagram</a>
-        <a href="https://www.facebook.com/babafirefive"> Facebook</a>
-        <a href="https://twitter.com/ZemmourEric"> Twitter</a>
+        <a href=""> Instagram</a>
+        <a href=""> Facebook</a>
+        <a href=""> Twitter</a>
         <h1> Nous trouver </h1>
         <p1> 28 rue Notre Dame des Champs </p1>
         <h1> Notre standing de qualité </h1>
-        <img src="WhatsApp Image 2022-03-15 at 19.30.05.jpeg" width="100" height="100">
+        <img src="photo-1604251405903-b8c4e83cdf7c.jpeg" width="1500" height="750">
     </div>
 
 </footer>
-<?php
-include "footer.php";
-?>
 </html>
