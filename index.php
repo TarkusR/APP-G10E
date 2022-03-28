4fbf3bd61d6b60ad09e723194436fd82b2b2d368
TarkusR <omisraou@gmail.com>
2022-03-28 15:07:07 +0200

    Added header for testing

--- index.php
+++ index.php
@@ -14,29 +14,6 @@ if(isset($_GET['logout']))
     
     <h1> <span>Bienvenue sur l'espace Care-Workplace </span> </h1>
     
-    <div id="zone">
-    <div> <img src="logomeasure.png" alt="logomeasure" width="200" height="180" id="logo">  </div>
-    <ul>
-        <li><a href="index.php" class="lienmenu">Accueil</a></li>
-        <li><a href="LeProduit.php" class="lienmenu">Le produit</a></li>
-        <li class="lienmenu">Espace Personnel</li>
-        <li class="lienmenu">Ludique</li>
-        <li class="lienmenu">FAQ</li>
-        <li class="lienmenu">Nous contactez</li>
-        <li class="lienmenu"> dégage</li>
-    </ul>
-
-        <?php
-        if(isset($_SESSION["loggedin"])){
-            echo ' <a class="monCompte lienmenu">Mon compte </a> ';
-            echo '<a href="logout.php" class="lienmenu">  <button class="connexion" >Déconnexion</button> </a> ';
-        } else{
-            echo ' <a  href="login.php" class="lienmenu">  <button class="connexion">Connexion</button> </a> ';
-        }
-
-
-        ?>
-
-    </div>
+    <?php  include "header.php"; ?>
 
 </body>
