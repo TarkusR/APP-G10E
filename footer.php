827e15ccaece936f1acb4981df4c0e0b8e1e3738
etiennegrandjean <stepan.grandjean@gmail.com>
2022-03-29 19:29:07 +0200

    Intégration du fichier style au login

--- footer.php
+++ footer.php
@@ -5,10 +5,11 @@
     <div class="footer">
         <h1> Contactez-nous </h1>
         <p class = "p">
-            <a href=""> Instagram</a> <br> <a href=""> Facebook</a> <br> <a href=""> Twitter</a>
+            <a href="" class = "f"> Instagram</a> <br> <a href="" class = "f"> Facebook</a> <br> <a href="" class = "f"> Twitter</a>
         </p>
         <h1 class = "h1"> Nous trouver </h1>
-        <p1 class = "p1"> <a href="https://www.google.fr/maps/place/ISEP/@48.8453262,2.3258358,17z/data=!3m1!4b1!4m5!3m4!1s0x47e671ce3fa0c01f:0x9bd1ac22c478d56e!8m2!3d48.8453227!4d2.3280245"> 28 rue Notre Dame des Champs </a> <br> 75006 <br> PARIS</p1>
+        <p1 class = "p1"> <a href="https://www.google.fr/maps/place/ISEP/@48.8453262,2.3258358,17z/data=!3m1!4b1!4m5!3m4!1s0x47e671ce3fa0c01f:0x9bd1ac22c478d56e!8m2!3d48.8453227!4d2.3280245" class = "f"> 28 rue Notre Dame des Champs </a> <br> 75006 <br> PARIS
+        </p1>
     </div>
 </footer>
 
