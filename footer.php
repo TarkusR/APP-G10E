f8728674ff7005afecedb941b4bff334a3f3bdbb
Nasri Samy <sana62205@eleve.isep.fr>
2022-04-02 14:54:45 +0200

    footer.php and css updated

--- footer.php
+++ footer.php
@@ -1,32 +1,30 @@
 <?php
 ?>
 
+
 <footer>
     <div class="footer">
 
-        <div>
-            <reseaux>
-                <h1> Nos reseaux </h1>
+            <div>
+                <h1 class="titreFooter"> Nos réseaux </h1>
                 <p class = "p">
                     <a href="" class = "f"> Instagram</a> <br> <a href="" class = "f"> Facebook</a> <br> <a href="" class = "f"> Twitter</a>
                 </p>
-            </reseaux>
-        </div>
+            </div>
 
-        <div>
-            <informations>
-                <h1 class = "h1"> Nous trouver </h1>
-                <p1 class = "p1"> <a href="https://www.google.fr/maps/place/ISEP/@48.8453262,2.3258358,17z/data=!3m1!4b1!4m5!3m4!1s0x47e671ce3fa0c01f:0x9bd1ac22c478d56e!8m2!3d48.8453227!4d2.3280245" class = "f"> 28 rue Notre Dame des Champs </a> <br> 75006 <br> PARIS</p1>
-            </informations>
-        </div>
+            <div>
+                <h1 class = "titreFooter"> Nous trouver </h1>
+                 <a href="https://www.google.fr/maps/place/ISEP/@48.8453262,2.3258358,17z/data=!3m1!4b1!4m5!3m4!1s0x47e671ce3fa0c01f:0x9bd1ac22c478d56e!8m2!3d48.8453227!4d2.3280245" class = "f"> 28 rue Notre Dame des Champs  <br> 75006 <br> PARIS </a>
+            </div>
 
-        <div>
-            <contact>
-                <h1> Nous contacter </h1>
-                <p> 06 99 16 87 78 <br> 01 68 82 33 12 <br> stepan.grandjean@gmail.com <br> </p>
-            </contact>
-        </div>
+            <div>
+                <h1 class="titreFooter"> Nous contacter </h1>
+                <p class="textFooter"> 06 99 16 87 78 <br> 01 68 82 33 12 <br> stepan.grandjean@gmail.com <br> </p>
+            </div>
 
     </div>
+
+
+
 </footer>
 
