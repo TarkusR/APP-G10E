95ae46b67e4ae0374785c5020b7dc6af9f5f8e55
Nasri Samy <sana62205@eleve.isep.fr>
2022-04-11 17:46:54 +0200

    css footer fixing

--- nousContactez.php
+++ nousContactez.php
@@ -1,4 +1,11 @@
 <?php
+session_start();
+if(isset($_SESSION["loggedin"])){
+
+} else{
+    header('Location:login.php');
+    exit();
+}
 ?>
 
 <!DOCTYPE html>
@@ -13,7 +20,11 @@
     <body>
     <?php  include "header.php"; ?>
 
-        <h1 class>Vous pouvez nous contactez en remplissant les informations si-dessous :</h1>
+        <h1 class>Vous pouvez nous contactez en remplissant les informations ci-dessous :</h1>
+    <label>Objet du message :</label>
+    <input class="" type="text">
+
+
 
     <?php include_once "footer.php" ?>
     </body>
