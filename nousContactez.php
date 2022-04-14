2bf3a1fddb725152ddeac159d3f7907ca7da6311
Nasri Samy <sana62205@eleve.isep.fr>
2022-04-14 11:51:01 +0200

    css + nousContactez updated

--- nousContactez.php
+++ nousContactez.php
@@ -21,15 +21,17 @@ if(isset($_SESSION["loggedin"])){
     <?php  include "header.php"; ?>
 
         <h1 class>Vous pouvez nous contactez en remplissant les informations ci-dessous :</h1>
+    <div class="">
     <div class="centreRegister">
     <label>Objet du message :</label>
     <input class="InputContact" type="text">
     </div>
-
         <p> Votre message : </p>
         <textarea rows="10" placeholder="Exprimez-vous" class="textContact"> </textarea>
-
-
+    <div class="centreRegister">
+        <button class="boutonConnexion">Envoyer</button>
+    </div>
+    </div>
     <?php include_once "footer.php" ?>
     </body>
 </html>
