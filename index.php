a357251b6c96e1b6fe7e0374ba188c7549912aef
Nasri Samy <sana62205@eleve.isep.fr>
2022-04-15 11:18:26 +0200

    accueil.php updated

--- index.php
+++ index.php
@@ -12,8 +12,10 @@ if(isset($_GET['logout']))
     <link rel="stylesheet" href="style.css">
 </head>
 <body class="accueil">
-
 <?php  include "header.php"; ?>
+<h1 class="centreRegister">Bienvenue sur l'espace Care-Workplace</h1>
+<p> Vous pouvez retrouver toutes les informations concernant notre produit et vos données en vous connectant avec votre compte Care-tech</p>
+
 
 <?php include_once "footer.php" ?>
 </body>
