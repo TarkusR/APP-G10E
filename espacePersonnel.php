9217493f3a6715a163d19ef2875c77df711e22d0
Nasri Samy <sana62205@eleve.isep.fr>
2022-04-15 11:28:10 +0200

    espace personnel session start required added

--- espacePersonnel.php
+++ espacePersonnel.php
@@ -1,5 +1,11 @@
 <?php
 session_start();
+if(isset($_SESSION["loggedin"])){
+
+} else{
+    header('Location:login.php');
+    exit();
+}
 ?>
     <!DOCTYPE html>
     <html lang="en">
@@ -8,10 +14,10 @@ session_start();
         <link rel="stylesheet" href="style.css">
     </head>
 <body>
-
 <?php  include "header.php"; ?>
 
-<?php  include "footer.php"; ?>
+<h1 class="centreRegister">Espace personnel</h1>
 
+<?php  include "footer.php"; ?>
 </body>
 </html>
