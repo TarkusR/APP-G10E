27b7d7e21b6a86024c18dd75f90e2e508fa40c90
ALegend4us <alexgendrot14@gmail.com>
2022-04-15 11:44:21 +0200

    New style for FAQ.php

--- FAQ.php
+++ FAQ.php
@@ -10,7 +10,7 @@ session_start();
 <body class="FAQbody">
 <?php  include "header.php"; ?>
     <h2>FAQ</h2>
-
+    <div class="dropdown">
     <button class="faq">Comment utiliser le capteur cardiaque?</button>
         <div class="reponse">
             <p>Une notice est fournie avec les capteurs cardiaques</p>
@@ -25,7 +25,7 @@ session_start();
         <div class="reponse">
             <p>Nous nous occupons de la maintenance tous les 3 ans</p>
         </div>
-
+    </div>
 <?php  include "footer.php"; ?>
 
 </body>
