97b9da2887cbb8c4bbc0ae0290227ea803fe4a9c
Nasri Samy <sana62205@eleve.isep.fr>
2022-04-15 11:23:53 +0200

    espace personnel creation

---
+++ espacePersonnel.php
@@ -0,0 +1,17 @@
+<?php
+session_start();
+?>
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <title>Espace Personnel</title>
+        <link rel="stylesheet" href="style.css">
+    </head>
+<body>
+
+<?php  include "header.php"; ?>
+
+<?php  include "footer.php"; ?>
+
+</body>
+</html>
