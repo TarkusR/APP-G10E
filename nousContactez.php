1b8cd8f0801ff893392a7199eb3d80d042035317
Oualid <omisraou@gmail.com>
2022-05-04 18:46:55 +0200

    Merged faq and le produit into a-propos.php

--- nousContactez.php
+++ nousContactez.php
@@ -20,20 +20,7 @@ if(isset($_SESSION["loggedin"])){
     <body>
     <?php  include "header.php"; ?>
 
-        <h1>Vous pouvez nous contacter en remplissant les informations ci-dessous :</h1>
-    <background
-    <div class="">
-    <div class="centreRegister">
-    <label>Objet du message :</label>
-    <input class="InputContact" type="text">
-    </div>
-        <p> Votre message : </p>
-        <textarea rows="10" placeholder=" Exprimez-vous" class="textContact"> </textarea>
-    <div class="centreRegister">
-        <br>
-        <button class="boutonContact">Envoyer</button>
-    </div>
-    </div>
+
     <?php include_once "footer.php" ?>
     </body>
 </html>
