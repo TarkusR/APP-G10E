74eb68d95d5ef21de72b443fda000e083abc37dc
babad <bdouchet92@gmail.com>
2022-05-06 08:51:13 +0200

    added content

--- index.php
+++ index.php
@@ -39,17 +39,36 @@ if(isset($_GET['logout']))
 </div>
 <div class="descriptor">
     <div class="descriptor-text-container">
-    <h1>TEST</h1>
-    <p class="descriptor-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vel bibendum dui, placerat accumsan turpis. Integer ante diam, eleifend eget blandit a, gravida ut eros. Mauris tincidunt, libero sit amet egestas convallis, leo risus porta arcu, a tincidunt turpis tortor vitae felis. Integer mattis enim non condimentum ornare. Duis bibendum massa tellus, a ornare tortor tempus quis. Vestibulum dictum faucibus ligula quis sagittis. Nam aliquam dolor nec felis pulvinar, id elementum felis luctus. Nam varius, orci ac accumsan fermentum, sapien elit varius mi, vitae tempus lorem arcu at sapien. Vestibulum hendrerit, tellus a pretium feugiat, massa nisi tempor ligula, non congue justo quam et nulla. Morbi finibus velit est, non tristique risus faucibus sit amet. Aliquam id pulvinar neque. Integer et euismod dolor, sed lacinia tortor. Aenean lobortis est vitae ipsum posuere, eu luctus dui volutpat. Nunc tempus ex a sapien ultricies pulvinar. Cras nisl lacus, imperdiet ac augue at, rhoncus pulvinar felis.</p>
+    <h1>Le bien-être au travail</h1>
+    <p class="descriptor-text">L’approche bien-être au travail vise à développer dans l’entreprise une conception de l’efficacité et de la performance qui soit soucieuse de la santé des salariés et favorise leur motivation et leur implication dans le travail. La mise en œuvre d’une politique de bien-être peut ainsi contribuer à l’épanouissement professionnel des salariés mais aussi à l’amélioration de l’ambiance de travail au sein des équipes et au renforcement du climat de respect et d’écoute. Elle contribue aussi à prévenir les risques psychosociaux, en amont des manifestations aiguës de stress, de violence ou d’épuisement.
+    </p>
     </div>
     <div class="descriptor-image-container" >
         <img class="descriptor-image" src="src/image-bureau.jpg" alt="">
     </div>
 </div>
+
+<!-- Ajout Baptiste -->
+
 <div class="consult-data-descriptor">
     <div class="consult-data-text">
-        <h1>Titre</h1>
-        <p> Sous titre </p>
+        <h1>Inspiré du sport de haut niveau</h1>
+    </div>
+    <div class="index-baptiste-image">
+        <img src="src/coeur.png" alt="">
+    </div>
+    <div class="index-baptiste-image">
+        <img src="src/air.png" alt="">
+    </div>
+
+</div>
+
+<!-- Fin ajout Baptiste -->
+
+<div class="consult-data-descriptor">
+    <div class="consult-data-text">
+        <h1>Vous souhaitez savoir si vous travaillez dans un environnement agréable?</h1>
+        <p> Cliquez juste ici </p>
     </div>
     <div class="consult-data-button">
         <a href="login.php"> <div id="btn"><span class="noselect">Consulter vos données</span><div id="circle"></div></div></a>
