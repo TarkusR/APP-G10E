3b083bc0e2cbac95637f8bef40424bf89b513652
ALegend4us <alexgendrot14@gmail.com>
2022-06-02 17:23:31 +0200

    Create CGU.php and add style.css

---
+++ CGU.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>CGU</title>
+    <link rel="stylesheet" href="style/style.css">
+</head>
+<body class="CGU_body">
+<h1 class="CGU_h1">Conditions générales d'utilisation</h1>
+<p>
+    En vigueur au 02/06/2022
+    Les présentes conditions générales d'utilisation (dites « CGU ») ont pour objet l'encadrement juridique
+    des modalités de mise à disposition du site et des services par CareTech et de définir les conditions
+    d’accès et d’utilisation des services par « l'Utilisateur ».
+    Les présentes CGU sont accessibles sur le site à la rubrique «CGU».</p>
+<h2 class="CGU_h2">Article 1 : Les mentions légales</h2>
+    <p>L'édition du site https://infinite-g10e.herogu.garageisep.com/ est assurée par la Société Start-up
+    CareTech au capital de 0 euros, immatriculée au RCS de Paris sous le numéro non précisé, dont le
+    siège social est situé au 28 rue notre dame des champs
+    Numéro de téléphone 0124532238
+    Adresse e-mail : CareTech@gmail.com.
+    Le Directeur de la publication est : Anonyme
+    L'hébergeur du site https://infinite-g10e.herogu.garageisep.com/ est la société Garage ISEP, dont le
+        siège social est situé au 10 rue de Vanves, avec le numéro de téléphone : non précisé.</p>
+<h2 class="CGU_h2">ARTICLE 2 : Accès au site</h2>
+    <p>Le site https://infinite-g10e.herogu.garageisep.com/ permet à l'Utilisateur un accès gratuit aux
+    services suivants :
+    Le site internet propose les services suivants :
+    Consulter les données environnementales de son espace de travail et son activité cardiaque.
+    Le site est accessible gratuitement en tout lieu à tout Utilisateur ayant un accès à Internet. Tous les
+    frais supportés par l'Utilisateur pour accéder au service (matériel informatique, logiciels, connexion
+        Internet, etc.) sont à sa charge.</p>
+<h2 class="CGU_h2">ARTICLE 3 : Collecte des données</h2>
+    <p>Le site est exempté de déclaration à la Commission Nationale Informatique et Libertés (CNIL) dans la
+        mesure où il ne collecte aucune donnée concernant les Utilisateurs.</p>
+<h2 class="CGU_h2">ARTICLE 4 : Propriété intellectuelle</h2>
+    <p>Les marques, logos, signes ainsi que tous les contenus du site (textes, images, son…) font l'objet
+    d'une protection par le Code de la propriété intellectuelle et plus particulièrement par le droit d'auteur.
+    1
+    L'Utilisateur doit solliciter l'autorisation préalable du site pour toute reproduction, publication, copie des
+    différents contenus. Il s'engage à une utilisation des contenus du site dans un cadre strictement privé,
+    toute utilisation à des fins commerciales et publicitaires est strictement interdite.
+    Toute représentation totale ou partielle de ce site par quelque procédé que ce soit, sans l’autorisation
+    expresse de l’exploitant du site Internet constituerait une contrefaçon sanctionnée par l’article L 335-2
+    et suivants du Code de la propriété intellectuelle.
+    Il est rappelé conformément à l’article L122-5 du Code de propriété intellectuelle que l’Utilisateur qui
+        reproduit, copie ou publie le contenu protégé doit citer l’auteur et sa source.</p>
+<h2 class="CGU_h2">ARTICLE 5 : Responsabilité</h2>
+    <p>Les sources des informations diffusées sur le site https://infinite-g10e.herogu.garageisep.com/ sont
+    réputées fiables mais le site ne garantit pas qu’il soit exempt de défauts, d’erreurs ou d’omissions.
+    Les informations communiquées sont présentées à titre indicatif et général sans valeur contractuelle.
+    Malgré des mises à jour régulières, le site https://infinite-g10e.herogu.garageisep.com/ ne peut être
+    tenu responsable de la modification des dispositions administratives et juridiques survenant après la
+    publication. De même, le site ne peut être tenue responsable de l’utilisation et de l’interprétation de
+    l’information contenue dans ce site.
+    Le site https://infinite-g10e.herogu.garageisep.com/ ne peut être tenu pour responsable d’éventuels
+    virus qui pourraient infecter l’ordinateur ou tout matériel informatique de l’Internaute, suite à une
+    utilisation, à l’accès, ou au téléchargement provenant de ce site.
+    La responsabilité du site ne peut être engagée en cas de force majeure ou du fait imprévisible et
+        insurmontable d'un tiers.</p>
+<h2 class="CGU_h2">ARTICLE 6 : Liens hypertextes</h2>
+    <p>Des liens hypertextes peuvent être présents sur le site. L’Utilisateur est informé qu’en cliquant sur ces
+    liens, il sortira du site https://infinite-g10e.herogu.garageisep.com/. Ce dernier n’a pas de contrôle sur
+    les pages web sur lesquelles aboutissent ces liens et ne saurait, en aucun cas, être responsable de
+        leur contenu.</p>
+<h2 class="CGU_h2">ARTICLE 7 : Cookies</h2>
+    <p>L’Utilisateur est informé que lors de ses visites sur le site, un cookie peut s’installer automatiquement
+    sur son logiciel de navigation.
+    Les cookies sont de petits fichiers stockés temporairement sur le disque dur de l’ordinateur de
+    l’Utilisateur par votre navigateur et qui sont nécessaires à l’utilisation du site https://infiniteg10e.herogu.garageisep.com/. Les cookies ne contiennent pas d’information personnelle et ne
+    peuvent pas être utilisés pour identifier quelqu’un. Un cookie contient un identifiant unique, généré
+    aléatoirement et donc anonyme. Certains cookies expirent à la fin de la visite de l’Utilisateur, d’autres
+    restent.
+    2
+    L’information contenue dans les cookies est utilisée pour améliorer le site https://infiniteg10e.herogu.garageisep.com/.
+    En naviguant sur le site, L’Utilisateur les accepte.
+    L’Utilisateur pourra désactiver ces cookies par l’intermédiaire des paramètres figurant au sein de son
+        logiciel de navigation.</p>
+<h2 class="CGU_h2">ARTICLE 8 : Droit applicable et juridiction compétente</h2>
+    <p>La législation française s'applique au présent contrat. En cas d'absence de résolution amiable d'un
+    litige né entre les parties, les tribunaux français seront seuls compétents pour en connaître.
+    Pour toute question relative à l’application des présentes CGU, vous pouvez joindre l’éditeur aux
+    coordonnées inscrites à l’ARTICLE 1.
+    CGU réalisées sur http://legalplace.fr/
+    3</p>
+
+
+</body>
+</html>
